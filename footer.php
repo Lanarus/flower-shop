93b6f6dfeb58cab390410dc7e67a552488b0ef95
Lanarus <ruslanasabitova0321@gmail.com>
2023-02-13 23:23:33 +0100

    footer css

--- footer.php
+++ footer.php
@@ -21,9 +21,9 @@
         <div class="box">
             <h3>contact info</h3>
             <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
-            <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
-            <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
-            <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 123-456 </p>
+            <p> <i class="fas fa-phone"></i> +111-222-1111 </p>
+            <p> <i class="fas fa-envelope"></i> lanarus@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> wroclaw, poland - 50-350 </p>
         </div>
 
         <div class="box">
