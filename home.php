6dcae2626032b1aee8e4a533c5a60a9bffe75096
Lanarus <ruslanasabitova0321@gmail.com>
2023-02-19 21:50:04 +0100

    view_page

--- home.php
+++ home.php
@@ -44,23 +44,32 @@
 
    <div class="box-container">
 
-     
-     <!--  <form action="" class="box">
-         
-         <div class="price">-</div>
-         <img src="" alt="" class="image">
-         <div class="name"></div>
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="">
-         <input type="hidden" name="product_name" value="">
-         <input type="hidden" name="product_price" value="">
-         <input type="hidden" name="product_image" value="">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-      </form> -->
-     
+<?php
+   $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+   if(mysqli_num_rows($select_products) > 0){
+      while($fetch_products = mysqli_fetch_assoc($select_products)){
+?>
+<form action="" method="POST" class="box">
+   <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+   <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+   <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+   <div class="name"><?php echo $fetch_products['name']; ?></div>
+   <input type="number" name="product_quantity" value="1" min="0" class="qty">
+   <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+   <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+   <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+   <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+   <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+   <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+</form>
+<?php
+   }
+}else{
+   echo '<p class="empty">no products added yet!</p>';
+}
+?>
 
-   </div>
+</div>
 
    <div class="more-btn">
       <a href="#shop" class="option-btn">load more</a>
