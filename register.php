408abe37c4a1a4a5dcc902f4601996975a247d0e
Lanarus <ruslanasabitova0321@gmail.com>
2023-02-21 11:05:17 +0100

    register.php

---
+++ register.php
@@ -0,0 +1,49 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>register</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+   
+<section class="form-container">
+
+   <form action="" method="post">
+      <h3>register now</h3>
+      <input type="text" name="name" class="box" placeholder="enter your username" required>
+      <input type="email" name="email" class="box" placeholder="enter your email" required>
+      <input type="password" name="pass" class="box" placeholder="enter your password" required>
+      <input type="password" name="cpass" class="box" placeholder="confirm your password" required>
+      <input type="submit" class="btn" name="submit" value="register now">
+      <p>already have an account? <a href="login.php">login now</a></p>
+   </form>
+
+</section>
+
+</body>
+</html>
