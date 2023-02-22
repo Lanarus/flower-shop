2f768e4004315d6afe0a97ca95df45483cc504b0
Lanarus <ruslanasabitova0321@gmail.com>
2023-02-22 17:19:02 +0100

    admin products css

--- admin_products.php
+++ admin_products.php
@@ -13,7 +13,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- custom admin css file link  -->
-   <link rel="stylesheet" href="css/admin_style.css">
+   <link rel="stylesheet" href="css/admin_style1.css">
 
 </head>
 <body>
