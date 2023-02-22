bd23b44caa8e1160e16d9fc961ea78bad0df771f
Lanarus <ruslanasabitova0321@gmail.com>
2023-02-22 08:47:54 +0100

    admin_page 1

---
+++ admin_page.php
@@ -0,0 +1,89 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+<body>
+   
+
+
+<section class="dashboard">
+
+   <h1 class="title">dashboard</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+        
+        
+         <p>total pendings</p>
+      </div>
+
+      <div class="box">
+        
+         <p>completed paymets</p>
+      </div>
+
+      <div class="box">
+         
+         <p>orders placed</p>
+      </div>
+
+      <div class="box">
+        
+         <p>products added</p>
+      </div>
+
+      <div class="box">
+         
+         <p>normal users</p>
+      </div>
+
+      <div class="box">
+         
+         <p>admin users</p>
+      </div>
+
+      <div class="box">
+         
+         <p>total accounts</p>
+      </div>
+
+      <div class="box">
+        
+         <p>new messages</p>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
