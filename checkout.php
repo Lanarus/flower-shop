3eea44bdd2d64bbad02a4b786451aaee3cb53e8a
Lanarus <ruslanasabitova0321@gmail.com>
2023-03-11 17:48:00 +0100

    checkout

---
+++ checkout.php
@@ -0,0 +1,102 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>checkout</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php @include 'header.php'; ?>
+
+<section class="heading">
+    <h3>checkout order</h3>
+    <p> <a href="home.php">home</a> / checkout </p>
+</section>
+
+<section class="display-order">
+   
+    <div class="grand-total">grand total : <span>$/-</span></div>
+</section>
+
+<section class="checkout">
+
+    <form action="" method="POST">
+
+        <h3>place your order</h3>
+
+        <div class="flex">
+            <div class="inputBox">
+                <span>your name :</span>
+                <input type="text" name="name" placeholder="enter your name">
+            </div>
+            <div class="inputBox">
+                <span>your number :</span>
+                <input type="number" name="number" min="0" placeholder="enter your number">
+            </div>
+            <div class="inputBox">
+                <span>your email :</span>
+                <input type="email" name="email" placeholder="enter your email">
+            </div>
+            <div class="inputBox">
+                <span>payment method :</span>
+                <select name="method">
+                    <option value="cash on delivery">cash on delivery</option>
+                    <option value="credit card">credit card</option>
+                    <option value="paypal">paypal</option>
+                    <option value="paytm">paytm</option>
+                </select>
+            </div>
+            <div class="inputBox">
+                <span>address line 01 :</span>
+                <input type="text" name="flat" placeholder="e.g. flat no.">
+            </div>
+            <div class="inputBox">
+                <span>address line 02 :</span>
+                <input type="text" name="street" placeholder="e.g.  streen name">
+            </div>
+            <div class="inputBox">
+                <span>city :</span>
+                <input type="text" name="city" placeholder="e.g. mumbai">
+            </div>
+            <div class="inputBox">
+                <span>state :</span>
+                <input type="text" name="state" placeholder="e.g. maharashtra">
+            </div>
+            <div class="inputBox">
+                <span>country :</span>
+                <input type="text" name="country" placeholder="e.g. india">
+            </div>
+            <div class="inputBox">
+                <span>pin code :</span>
+                <input type="number" min="0" name="pin_code" placeholder="e.g. 123456">
+            </div>
+        </div>
+
+        <input type="submit" name="order" value="order now" class="btn">
+
+    </form>
+
+</section>
+
+
+
+
+
+
+<?php @include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
